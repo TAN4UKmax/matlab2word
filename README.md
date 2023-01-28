9cc7c5a917df8564842b2f37e5d006cab6207a8f
TAN4UKmax <tan4ukmak7@gmail.com>
2023-01-28 17:51:11 +0300

    v1.1.0 update

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # matlab2word
-A MATLAB library that allows to write your calculations into MS Word template file.
+A MatLAB library that allows to write your calculations into Word template file.
 It might be useful when you have a report template which has specific formatting and requires some calculations.
-See how to use this library in files matlab2word_example_en.mlx or matlab2word_example_ru.mlx (open these files in MATLAB).
+See how to use this library in files matlab2word_example_en.mlx or matlab2word_example_ru.mlx (open these files in MatLAB).
+
+
+Version history
+
+v1.0.0 - Initial version.
+-   Abilities to write text, numbers and plots into Word
+
+v1.1.0 - Added some features
+-   Now your result saves automatically by adding _out part to your input file.
+If you want to select a place to save, use method .SaveManually()
+-   You can change your default imaginary unit using function .SetImaginaryUnit('j')
+-   You can use data for replacement not only from MATLAB, but also from other file
--- matlab2word.m
+++ matlab2word.m
@@ -18,6 +18,7 @@ classdef matlab2word < handle
     % text <number_example> text
     % Figure:
     % <fig_example>
+    % <paste_file_example>
     %
     %   Save your document in *.docx format and close Word application
     %   Then launch the following script in the same folder:
@@ -34,11 +35,15 @@ classdef matlab2word < handle
     % %% Write file %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % % Create an instance of matlab2word object and get access to Word file
     % m2w = matlab2word();
+    % % Set decimal separator and imaginary unit if needed
     % m2w.SetDecimalSeparator('comma');
+    % m2w.SetImaginaryUnit('j');
     % % Replace variables
     % m2w.Replace('string_example', test_string);
     % m2w.Replace('number_example', test_num);
     % m2w.Replace('fig_example', fig1);
+    % % Replace data from other file
+    % m2w.PasteFile('paste_file_example', 'template1.docx');
     % % Save Word file
     % m2w.Save();
     %
@@ -70,9 +75,12 @@ classdef matlab2word < handle
     properties (SetAccess=private)
         % Stores decimal separetor parameter ('dot' by default)
         decimalSeparator
+        % Stores imaginary unit parameter ('i' by default)
+        imaginaryUnit
     end
     
     properties (SetAccess=private, Hidden)
+        %     properties
         % Stores input file parameters
         inFileSpec
         % Stores Word instance
@@ -115,12 +123,15 @@ classdef matlab2word < handle
             this.word.Visible =1; % shows Word window
             this.document = this.word.Documents.Open(this.inFileSpec);
             this.selection = this.word.Selection;
+            
             % Set default decimal separator to dot
             this.decimalSeparator = 'dot';
+            % Set default imaginary unit to i
+            this.imaginaryUnit = 'i';
         end
         
         function Replace(this, replace_id, replace_data)
-            %REPLACE Replaces data in Word
+            %Replace Replaces data in Word
             %   Replace method replaces <replace_id> string in Word
             %   document by replace_data. replace_data can be string,
             %   number or figure instance.
@@ -145,6 +156,7 @@ classdef matlab2word < handle
                         float_str = replace(float_str, '.', ',');
                     end
                     % Fix position of imaginary unit in small or large numbers
+                    % (for example replace '3,86e-18-1,3e-11i' by '3,86e-18-1,3ie-11')
                     if (~isreal(replace_data) && (contains(num2str(imag(replace_data)), 'e')))
                         e_index = numel(float_str);
                         while (float_str(e_index) ~= 'e')
@@ -153,18 +165,29 @@ classdef matlab2word < handle
                         float_str = [float_str(1:(e_index-1)), 'ie', ...
                             float_str((e_index+1):(end-1))];
                     end
+                    % Replace i by j here for imaginary units if needed
+                    if strcmp(this.imaginaryUnit, 'j')
+                        float_str = replace(float_str, 'i', 'j');
+                    end
+                    % Prints different types of number
                     if contains(float_str, 'e')
                         % Replace e by power of 10
                         float_str = replace(float_str, 'e', '⋅10^');
+                        this.selection.TypeText(' ');
+                        this.selection.TypeBackspace();
+                        this.selection.OMaths.Add(this.selection.Range);
                         this.selection.TypeText(float_str);
                         this.selection.MoveLeft(1, length(float_str), 1);
                         %1=character mode
                         %with this command we mark the previous text%length(text)=amount
                         %1=hold shift
-                        this.selection.OMaths.Add(this.selection.Range);
+                        % For correct imaginary unit replace
+                        this.selection.Font.Italic = 0;
                         this.selection.OMaths.BuildUp();
                     else
                         this.selection.TypeText(float_str);
+                        this.selection.MoveLeft(1, length(float_str), 1);
+                        this.selection.Font.Italic = 0; %
                     end
                 else
                     if isgraphics(replace_data)
@@ -186,14 +209,67 @@ classdef matlab2word < handle
             end
         end
         
+        function PasteFile(this, replace_id, file_name)
+            %PasteFile Can replace by whole file
+            
+            % Open file for replace
+            replace_file = matlab2word(file_name);
+            
+            % Add brackets to search  string
+            findString = ['<', replace_id, '>'];
+            find = this.selection.Find;
+            % Search parameters setup
+            find.ClearFormatting();
+            find.Replacement.ClearFormatting();
+            find.Text = findString;
+            find.Wrap = 1;
+            find.MatchCase = true;
+            % Search
+            find.Execute();
+            while find.Found % If found something
+                replace_file.selection.WholeStory();
+                replace_file.selection.Copy();
+                % 16 = wdFormatOriginalFormatting
+                this.selection.PasteAndFormat(16);
+                
+                % Make search one more time to find all instances
+                find.ClearFormatting();
+                find.Replacement.ClearFormatting();
+                find.Text = findString;
+                find.Wrap = 1;
+                find.MatchCase = true;
+                find.Execute();
+            end
+            delete(replace_file);
+        end
+        
         function Save(this)
-            %SAVE Saves Word file
+            %Save Saves Word file
+            %   This method should called in the end of file to save it
+            
+            % Prepare out file name and extention
+            [outFilePath, outFileName, fileExt] = fileparts(this.inFileSpec);
+            outFileName = append(outFileName, '_out', fileExt);
+            % make output path
+            outFileSpec = fullfile(outFilePath, outFileName);
+            % Delete old report
+            if isfile(outFileSpec)
+                delete(outFileSpec);
+            end
+            % Save new document
+            this.document.SaveAs2(outFileSpec);
+            delete(this);
+        end
+        
+        function SaveManually(this)
+            %SaveManually Saves Word file with asking user for file name
+            %and path
             %   This method should called in the end of file to save it
             
             % Prepare out file name and extention
             [~, outFileName, fileExt] = fileparts(this.inFileSpec);
             outFileName = append(outFileName, '_out', fileExt);
-            % Ask user to confirm output file
+            % Ask user to confirm output file name or select another one
             [outFileName, outFilePath] = uiputfile( ...
                 {'*.docx', 'Word Documents (*.docx)'}, ...
                 'Save As', ...
@@ -210,7 +286,6 @@ classdef matlab2word < handle
             end
             % Save new document
             this.document.SaveAs2(outFileSpec);
-            
             delete(this);
         end
         
@@ -226,6 +301,17 @@ classdef matlab2word < handle
             end
         end
         
+        function SetImaginaryUnit(this, unit)
+            %SetImaginaryUnit Sets imaginary unit for complex numbers
+            %   'i' unit is set by default. You can change separator
+            %   to 'j  by calling this method with argument 'j'
+            if (nargin == 2) && strcmpi(unit, 'j')
+                this.imaginaryUnit = 'j';
+            else
+                this.imaginaryUnit = 'i';
+            end
+        end
+        
         function delete(this)
             %DELETE Destructor of class
             %   Closes Word and deletes its instance
@@ -234,7 +320,6 @@ classdef matlab2word < handle
             this.word.Quit();       % Close Word app
             % Delete Word object (it also deletes all related instances)
             delete(this.word);
-            close all;              % Close all opened figures
         end
         
     end
